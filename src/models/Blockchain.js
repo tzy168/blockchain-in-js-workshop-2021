15bf110204db929e8eed0379fa956efceb9d3901
tzy168 <3265577395@qq.com>
2023-06-02 08:49:00 +0800

    2

--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -1,35 +1,21 @@
-import UTXOPool from './UTXOPool.js'
+import UTXO from "../models/UTXO.js"
 
+// Blockchain
 class Blockchain {
   // 1. 完成构造函数及其参数
-  /* 构造函数需要包含
+  /* 构造函数需要包含 
       - 名字
       - 创世区块
       - 存储区块的映射
   */
   constructor(name) {
+    this.blocks=[]
     this.name=name
     this.genesis=null
-    this.blocks=[]
-  }
+    }
+    
 
   // 2. 定义 longestChain 函数
-  /*
-    返回当前链中最长的区块信息列表
-  */
-
-  // 判断当前区块链是否包含
-
-  // 获得区块高度最高的区块
-
-  // 添加区块
-  /*
-
-  */
-
-
-
-  // 2. 定义 longestChain 函数s
   /* 
     返回当前链中最长的区块信息列表
   */
@@ -38,6 +24,7 @@ class Blockchain {
       let maxHeight = Math.max(...Object.values(this.blocks).map(block => block.height))
       let currentBlock = Object.values(this.blocks).find(block => block.height === maxHeight)
       let longestChain = []
+  
       while (currentBlock) {
         longestChain.push(currentBlock)
         currentBlock = Object.values(this.blocks).find(
@@ -73,11 +60,8 @@ class Blockchain {
       
     }
     
-      
-      
     }
-  
+    
  
-
 
 export default Blockchain
